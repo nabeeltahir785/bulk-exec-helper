8b77567b3c6273211d2a816e520b082d91256718
Muhammad Nabeel Mahmood <nabeel.mehmood@jeeny.me>
2025-02-14 19:51:31 +0500

    perf: optimize bulk execution with atomic counter and pre-allocation

--- bulk-exec-helper.class.ts
+++ bulk-exec-helper.class.ts
@@ -1,83 +1,70 @@
 import { Readable } from "node:stream";
 
-type ExecFn<T> = (...arg0: T[]) => Promise<unknown>;
+type ExecFn<T> = (...args: T[]) => Promise<unknown>;
 
 export class BulkExecHelper<T> {
   private execResult: unknown[];
-
   private execFn: ExecFn<T>;
-
   private fnArgs: Array<Array<T>>;
-
-  private indicesRemaining: number[];
-
+  private nextIndex: number;
   public resultStream: Readable;
-
   private resultsEmitted: number;
-
   private isStream: boolean;
 
   constructor(execFn: ExecFn<T>, args: Array<Array<T>>) {
     this.execFn = execFn;
     this.fnArgs = args;
-    this.indicesRemaining = args.map((_, i) => i);
+    this.nextIndex = 0;
     this.resultStream = new Readable({
       objectMode: true,
       read: () => {},
     });
-    this.execResult = [];
+    this.execResult = new Array(args.length);
     this.resultsEmitted = 0;
     this.isStream = false;
+    this.processNextIndex = this.processNextIndex.bind(this);
   }
 
   public async exec(parallelExecs: number): Promise<unknown[]> {
     this.resultStream.destroy();
-    const promiseChains = new Array(parallelExecs).fill(null);
+    const promises = Array.from({ length: parallelExecs }, () => this.processNextIndex());
 
-    for (let i = 0; i < parallelExecs; i++) {
-      promiseChains[i] = this.processNextIndex();
-    }
-
-    return Promise.all(promiseChains)
-      .then(() => this.execResult)
-      .catch((err) => {
-        throw err;
-      });
+    return Promise.all(promises)
+        .then(() => this.execResult)
+        .catch((err) => {
+          throw err;
+        });
   }
 
   public execStream(parallelExecs: number): void {
     this.isStream = true;
+    const promises = Array.from({ length: parallelExecs }, () => this.processNextIndex());
 
-    const promiseChains = new Array(parallelExecs).fill(null);
-
-    for (let i = 0; i < parallelExecs; i++) {
-      promiseChains[i] = this.processNextIndex();
-    }
-
-    Promise.all(promiseChains)
-      .then(() => this.execResult)
-      .catch((err) => {
-        this.resultStream.destroy(err);
-      });
+    Promise.all(promises).catch((err) => {
+      this.resultStream.destroy(err);
+    });
   }
 
-  private processResults(index: number, result: unknown) {
+  private processResults(index: number, result: unknown): void {
     if (this.isStream) {
       this.resultsEmitted++;
       this.resultStream.push(result);
-      if (this.resultsEmitted === this.fnArgs.length)
+      if (this.resultsEmitted === this.fnArgs.length) {
         this.resultStream.push(null);
+      }
     } else {
       this.execResult[index] = result;
     }
   }
 
   private processNextIndex(): Promise<unknown> {
-    const index = this.indicesRemaining.shift();
-    if (index === undefined) return Promise.resolve(null);
+    const index = this.nextIndex++;
+    if (index >= this.fnArgs.length) {
+      return Promise.resolve(null);
+    }
 
     return this.execFn(...this.fnArgs[index])
-      .then(this.processResults.bind(this, index))
-      .then(this.processNextIndex.bind(this));
+        .then((result) => this.processResults(index, result))
+        .then(this.processNextIndex);
   }
 }
